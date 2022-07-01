1dece103b9619e1f4018b37f4b942b02c78cd8c5
yuto1108 <yuto-1108@au.com>
2022-07-01 13:11:27 +0900

    削除機能の修正

--- app/models/camp.rb
+++ app/models/camp.rb
@@ -3,7 +3,7 @@ class Camp < ApplicationRecord
   belongs_to :prefecture
   has_one_attached :image
   belongs_to :user
-  has_many :comments
+  has_many :comments, dependent: :destroy
   with_options presence: true do
     validates :name
     validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
